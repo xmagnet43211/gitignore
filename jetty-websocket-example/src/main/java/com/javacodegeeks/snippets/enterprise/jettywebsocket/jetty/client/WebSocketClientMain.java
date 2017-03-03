14abe26b0dcc1807a0ab4c390a1afd0673f39cde
xmagnet43211 <giannousisx.teipir@gmail.com>
2017-03-03 13:57:43 +0200

    second commit

---
+++ jetty-websocket-example/src/main/java/com/javacodegeeks/snippets/enterprise/jettywebsocket/jetty/client/WebSocketClientMain.java
@@ -0,0 +1,38 @@
+package com.javacodegeeks.snippets.enterprise.jettywebsocket.jetty.client;
+
+import java.net.URI;
+
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+
+import static org.eclipse.jetty.http.HttpParser.State.URI;
+
+public class WebSocketClientMain {
+
+	public static void main(String[] args) {
+		String dest = "ws://localhost:8080/toUpper";
+		WebSocketClient client = new WebSocketClient();
+		try {
+			
+			ToUpperClientSocket socket = new ToUpperClientSocket();
+			client.start();
+			URI echoUri = new URI(dest);
+
+			ClientUpgradeRequest request = new ClientUpgradeRequest();
+			client.connect(socket, echoUri, request);
+			socket.getLatch().await();
+			socket.sendMessage("echo");
+			socket.sendMessage("test");
+			Thread.sleep(10000l);
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			try {
+				client.stop();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
