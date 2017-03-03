76cc499967a306b39928fc683b1b9bc4ccd2195a
xmagnet43211 <giannousisx.teipir@gmail.com>
2017-03-03 14:17:20 +0200

    third commit

---
+++ Spring4WebSocket/src/main/java/com/concretepage/controller/WebSocketController.java
@@ -0,0 +1,27 @@
+package com.concretepage.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.concretepage.vo.CalcInput;
+import com.concretepage.vo.Result;
+
+@Controller
+public class WebSocketController {
+
+
+	@MessageMapping("/add" )
+    @SendTo("/topic/showResult")
+    public Result addNum(CalcInput input) throws Exception {
+        Thread.sleep(2000);
+        Result result = new Result(input.getNum1()+"+"+input.getNum2()+"="+(input.getNum1()+input.getNum2())); 
+        return result;
+    }
+    
+    @RequestMapping("/start")
+    public String start() {
+        return "start";
+    }
+}
